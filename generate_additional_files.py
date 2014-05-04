a4d60d654211435ce35263efab1f77c6a47c3b2c
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2014-05-04 15:48:58 +0100

    Updated additional files

--- generate_additional_files.py
+++ generate_additional_files.py
@@ -1,52 +1,24 @@
 #!/usr/bin/python
 
-import os, gettext
-
 DOMAIN = "cinnamon-bluetooth"
 PATH = "/usr/share/cinnamon/locale"
 
-def generate(filename, prefix, name, comment, suffix):
-    gettext.install(DOMAIN, PATH)
-    desktopFile = open(filename, "w")
+import os, gettext, sys
+sys.path.append('/usr/lib/linuxmint/common')
+import additionalfiles
 
-    desktopFile.writelines(prefix)
-
-    desktopFile.writelines("Name=%s\n" % name)
-    for directory in sorted(os.listdir(PATH)):
-        if os.path.isdir(os.path.join(PATH, directory)):
-            try:
-                language = gettext.translation(DOMAIN, PATH, languages=[directory])
-                language.install()          
-                desktopFile.writelines("Name[%s]=%s\n" % (directory, _(name)))
-            except:
-                pass
-
-    desktopFile.writelines("Comment=%s\n" % comment)
-    for directory in sorted(os.listdir(PATH)):
-        if os.path.isdir(os.path.join(PATH, directory)):
-            try:
-                language = gettext.translation(DOMAIN, PATH, languages=[directory])
-                language.install()                      
-                desktopFile.writelines("Comment[%s]=%s\n" % (directory, _(comment)))
-            except:
-                pass
-
-    desktopFile.writelines(suffix)
-
-os.environ['LANG'] = "en"
+os.environ['LANG'] = "en_US.UTF-8"
 gettext.install(DOMAIN, PATH)
 
 prefix = """[Desktop Entry]
-Icon=cinnamon-bluetooth
+Icon=bluetooth
 Exec=cinnamon-settings bluetooth
 Terminal=false
 Type=Application
 Categories=GTK;Settings;X-Cinnamon-NetworkSettings;HardwareSettings;X-Cinnamon-Settings-Panel;
-OnlyShowIn=GNOME;
+OnlyShowIn=X-Cinnamon;
 StartupNotify=true
 X-Cinnamon-Settings-Panel=bluetooth
 """
 
-generate("./panels/bluetooth/cinnamon-bluetooth-properties.desktop.in.in", prefix, _("Bluetooth"), _("Configure Bluetooth settings"), "")
-
-
+additionalfiles.generate(DOMAIN, PATH, "./panels/bluetooth/cinnamon-bluetooth-properties.desktop.in.in", prefix, _("Bluetooth"), _("Configure Bluetooth settings"), "")
