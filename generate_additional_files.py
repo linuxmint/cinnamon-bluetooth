bcbc994e68f8cd6986d38b5a8eeed1fc46f15f1e
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2014-10-31 13:26:53 +0100

    Switched locale path to standard /usr/share/locale

--- generate_additional_files.py
+++ generate_additional_files.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 DOMAIN = "cinnamon-bluetooth"
-PATH = "/usr/share/cinnamon/locale"
+PATH = "/usr/share/locale"
 
 import os, gettext, sys
 sys.path.append('/usr/lib/linuxmint/common')
